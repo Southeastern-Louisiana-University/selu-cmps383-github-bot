a06c1fd5a6a95555e586076d4e083e5a8b8dd0b9
matthewvidacovich <194949094+matthewvidacovich@users.noreply.github.com>
2025-03-04 10:09:47 -0600

    feat: automatically delete expensive databases

---
+++ Selu383Bot/Functions/DeleteExpensiveDatabases.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Selu383Bot.Features.Azure;
+using Selu383Bot.Helpers;
+
+namespace Selu383Bot.Functions;
+
+public static partial class DeleteExpensiveDatabases
+{
+    [GeneratedRegex("GP_S_Gen.*_1")]
+    private static partial Regex GeneralPurposeServerless();
+
+    [Function("DeleteExpensiveDatabases")]
+    public static async Task RunAsync([TimerTrigger("0 0 * * * *")] TimerInfo myTimer, ILogger log)
+    {
+        var ownerServicePrincipal = JsonConvert.DeserializeObject<ServicePrincipalData>(FunctionHelper.GetEnvironmentVariable("AzureServicePrincipalData")) ?? throw new Exception("missing AzureServicePrincipalData");
+        var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(ownerServicePrincipal.AppId, ownerServicePrincipal.Password, ownerServicePrincipal.Tenant, AzureEnvironment.AzureGlobalCloud);
+
+        var authenticated = Microsoft.Azure.Management.Fluent.Azure.Authenticate(credentials);
+        var azure = authenticated.WithSubscription(ownerServicePrincipal.SubscriptionId);
+
+
+        var servers = new List<(string id, string slo)>();
+
+        var resourceGroups = await azure.ResourceGroups.ListAsync();
+        while (resourceGroups != null && resourceGroups.Any())
+        {
+            var studentGroups = resourceGroups.Where(x => x.Id.Contains("/cmps383")).ToArray();
+            foreach (var resourceGroup in studentGroups)
+            {
+                var sqlServers = azure.SqlServers.ListByResourceGroup(resourceGroup.Name);
+                foreach (var server in sqlServers)
+                {
+                    var databases = await server.Databases.ListAsync();
+                    foreach (var database in databases)
+                    {
+                        if(database.Name == "master")
+                        {
+                            continue;
+                        }
+                        servers.Add(new (server.Id, database.ServiceLevelObjective.ToString()));
+                    }
+                }
+            }
+
+            resourceGroups = await resourceGroups.GetNextPageAsync();
+        }
+
+        var gp = GeneralPurposeServerless();
+        var toDelete = servers
+            .Where(s => s.slo != "Basic" && !gp.IsMatch(s.slo))
+            .Select(s => s.id)
+            .ToArray();
+
+        if (toDelete.Any())
+        {
+            log.LogInformation($"Deleting expensive databases: {JsonConvert.SerializeObject(toDelete)}");
+
+            await azure.SqlServers.DeleteByIdsAsync(toDelete);
+        }
+        else
+        {
+            log.LogInformation($"No expensive DBs: {JsonConvert.SerializeObject(servers)}");
+        }
+    }
+}
